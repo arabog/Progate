2de5062b6824188876b3f9f17442044acc4a02a6
arabambi <arababs2015@gmail.com>
2022-01-24 10:40:06 +0100

    progate started

---
+++ React/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+/* Diff btw JS & JSX:
+Js is written above return while JSX is written
+inside return
+
+to write JS inside return put it in {}
+
+
+
+*/ 
+
+function App() {
+	// JS
+
+
+	return (
+		<div className="App">
+			{/* JSX */} Hello
+		</div>
+	);
+}
+
+export default App;
