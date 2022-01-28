b9690faf87ccc78721166d08dc6a4d77a6308258
arabambi <arababs2015@gmail.com>
2022-01-28 09:37:41 +0100

    React 1 completed

--- React/src/App.js
+++ React/src/App.js
@@ -9,6 +9,25 @@ inside return
 to write JS inside return put it in {}
 
 
+To change what is displayed when someone clicks a 
+button, you need 2 tools, events and state.
+
+Event:
+<button eventName={() => {code}}> </button>
+
+State:
+A value that changes depending on a user's actions is 
+called state in React
+
+Updating Pages with State involves 3 steps:
+def state
+display state
+update state
+
+using class component
+this.state = {
+	name: 'Ken the Ninja'
+}
 
 */ 
 
@@ -19,6 +38,8 @@ function App() {
 	return (
 		<div className="App">
 			{/* JSX */} Hello
+
+			{console.log('Hello')}
 		</div>
 	);
 }
