818a59772bbf2c0904890ba4df6be318822df40d
arabambi <arababs2015@gmail.com>
2022-01-30 14:23:16 +0100

    ReactIII done

--- React/src/App.js
+++ React/src/App.js
@@ -101,6 +101,72 @@ constructor(props) {
 	}
 }
 
+
+// js inside render() {}
+if (this.state.isModalOpen) {
+{
+	modal = (
+		<div className='modal'>
+			<div className='modal-inner'>
+				<div className='modal-header'></div>
+				
+				<div className='modal-introduction'>
+				
+				<h2>{this.props.name}</h2>
+				
+				<p>{this.props.introduction}</p>
+			</div>
+
+			<button className='modal-close-btn'>
+				Close
+			</button>
+		</div>
+	</div>  
+)
+
+}
+}
+
+
+handleClickLesson() {
+    return this.setState({isModalOpen: true});
+  }
+  
+  render() {
+    let modal;
+    if (this.state.isModalOpen) {
+      modal = (
+        <div className='modal'>
+          <div className='modal-inner'>
+            <div className='modal-header'></div>
+            <div className='modal-introduction'>
+              <h2>{this.props.name}</h2>
+              <p>{this.props.introduction}</p>
+            </div>
+            <button 
+              className='modal-close-btn'
+              onClick={() => {this.handleClickLesson()}}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className='lesson-card'>
+        <div
+          className='lesson-item'
+          onClick={() => {this.handleClickLesson()}}
+        >
+          <p>{this.props.name}</p>
+          <img src={this.props.image} />
+        </div>
+        {modal}
+      </div>
+    );
+  }
 */ 
 
 function App() {
