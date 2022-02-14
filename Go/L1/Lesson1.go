91572f11517e7248ac3aead597d26c59f10b1be9
arabambi <arababs2015@gmail.com>
2022-02-14 22:45:21 +0100

    run a go file

---
+++ Go/L1/Lesson1.go
@@ -0,0 +1,29 @@
+// package main
+
+// func main() {		
+// 	println("Hello World")
+	
+// }
+
+// package main
+
+// func main() {
+// 	s1 := "Hi, "
+// 	s2 := "world"
+	
+// 	s1 = "Hello, "
+
+// 	println(s1, s2)
+
+// }
+
+package main
+
+func main() {
+	n := 10
+	n =  n + 20
+	
+	// n += 20
+	
+	println(n)	//30
+}
--- Go/info.go
+++ Go/info.go
@@ -1,4 +1,7 @@
 /*
+go run filename
+
+
 What is Go?
 Go is a programming language produced by Google. With a 
 simple grammar, it's easy to learn and well-suited for team 
@@ -90,10 +93,292 @@ func main() {
 }
 
 
+Doing Calculations with Go:
+Other calculations besides addition and subtraction can be 
+done with Go. For example, * is used for multiplication and 
+/ for division. Also, % can be used to calculate the remainder 
+after division. These symbols are known as operators. 
 
+
+Combining Strings
+In Go, you can perform operations other than numbers as well. 
+The + operator is used to add numbers, but when used on strings, 
+it combines them into a single string. Combining strings is known 
+as string concatenation. In the example below, two strings Hi, and 
+Gophers are concatenated by the + operator to form a single string; 
+Hi,Gophers.
+
+package main
+
+func main() {
+	println("Hi" + "Gophers") 	
+	println("2" + "3")	// 23
+}
+
+
+What are Variables?
+Now we'll learn about variables. You can think of a variable as a 
+box where you can store a value. Also, the box (variable) has a 
+name which can be used to retrieve a value from the variable 
+anytime.
+
+Variables and Data Types
+Variables must be defined before you can use them. To define 
+a variable, you can write var variableName dataType as shown 
+below. e.g
+
+var number int
+
+var nme string
+
+
+Using Variables
+The code n = 100 means assign the value 100 to the variable n. 
+The = operator in Go does not mean "equal". This assignment 
+operator means assign the value on the right to the variable 
+on the left. 
+
+
+package main
+
+func main() {
+	var n int
+
+	n = 100	// assign 100 to d n variable
+		
+	println(n)	// d variable n contains 100
+}
+
+
+Declaration and Assignment
+By writing var n int = 100, you can declare a variable and assign 
+it a value simultaneously.
+
+package main
+
+func main() {
+	var n int = 100	// declare & assign 100 to d n variable
+
+	println(n)	// d variable n contains 100
+}
+
+Updating Variables
+Variables can be updated as well. In order to update variables, 
+new values must be assigned using the = operator
+
+
+package main
+
+func main() {
+	var n int = 100	
+	println(n)	//100
+
+	n = 200	// update n value to 200
+	println(n)	//200
+}
+
+
+Omitting Data Types
+In Go, when variable declaration and value assignment are done 
+at the same time, like var a int = 100, the data type can be omitted. 
+In the example, int is left out so that only var a = 100 is written. 
+This is because as 100 is obviously an integer, it can be safely 
+assumed that the data type assigned to the variable a is an integer.
+
+
+package main
+
+func main() {
+	var a  = 100	
+	println(a)	//100
+
+}
+
+Abbreviating Variable Definitions
+Variable definitions can be written without a data type by using a 
+shortened syntax. As shown below, var b int = 200 and b := 200 
+are the same. Note that there's a colon : before the = symbol (:=). 
+
+package main
+
+func main() {
+	b := 200	// d same as var b int = 200
+	println(b)	//200
+
+}
+
+Other Ways to Use println
+There is another useful way to use println. In the example 
+below, the variables a and b used with println are separated 
+by a comma , in the (). 
+
+package main
+
+func main() {
+	a := 100
+	b := 200	
+
+	println(a, b)	//print 2 variables
+
+}
+
+Variable Definitions and Assignment
+Generally in programming, the same variable name cannot be 
+declared multiple times. In the image on the left, the a variable 
+is declared twice, which results in an error. Note the difference 
+between := for variable declarations with assignment and = 
+for value assignment.
+
+package main
+
+func main() {
+	a := 100
+	a := 200	 // <--Error!
+
+	println(a)
+
+}
+
+package main
+
+func main() {
+	s1 := "Hi, "
+	s2 := "world"
+	
+	s1 = "Hello, "
+
+	println(s1, s2)
+
+}
+
+
+Where Variables Can Be Used
+Variables can only be used after they are defined. In the example 
+below, the n variable is used before it is defined.
+
+package main
+
+func main() {
+	println(a)	//wrong
+
+	a := 100
+
+	println(a)
+
+}
+
+
+Unused Variables
+In Go, when variables are declared but are left unused, errors 
+are displayed. 
+
+package main
+
+func main() {
+	println(a)	//wrong
+
+	a := 100
+	b := 100
+
+	println(a)
+
+}
+
+
+Type Mismatch
+Values of a different data type than the data type of the 
+variable cannot be assigned. As shown in the left image, 
+if we assign a value of type string to a variable of type 
+int (n := 100), we'll get an error message.
+
+package main
+
+func main() {
+
+	a := 100
+	b := "Hello" Not an int type
+
+	println(a)
+}
+
+package main
+
+func main() {
+	s1 := "Hi, "
+	s2 := "world"
+	
+	s1 = "Hello, "
+
+	println(s1, s2)
+
+}
+
+Calculations with Variables
+Since variables can be treated like values, we can also use 
+variables to perform operations.
+
+package main
+
+func main() {
+	n := 10
+	
+
+	println(n + 20)	//30
+
+}
+
+
+package main
+
+func main() {
+	n := 10
+	m := 20
+
+	println(n + m)	//30
+
+}
+
+Self-Assignment
+Below, on the left, n = n + 20 may seem strange when you 
+think of it in a mathematical way. However, that's not a 
+problem because in Go, = stands for assign not equal.
+
+package main
+
+func main() {
+	n := 10
+	n =  n + 20
+
+	// n += 20
+
+	println(n)	//30
+
+}
+
+
+Shortened Self-Assignment
+n += 20
+n -= 20
+n *= 20
+n /= 20
+n %= 20
+
+n = n + 1 --> n+= 1 --> n++
+n = n - 1 --> n -= 1 --> n--
 
 
 
 #Progate @progateEN @dev_careers #dev_careers
 
+package main
+
+func main() {
+	n := 10
+	n =  n + 20
+	
+	// n += 20
+	
+	println(n)	//30
+	
+}
 */ 
