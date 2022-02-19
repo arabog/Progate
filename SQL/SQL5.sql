f56e045693c1935692f1273ddc0d951e9fe5f00a
arabambi <arababs2015@gmail.com>
2022-02-19 17:46:18 +0100

    SQL L5 insert done

--- SQL/SQL5.sql
+++ SQL/SQL5.sql
@@ -1,10 +1,17 @@
+/* 
 SQL Data Processing
 In this lesson, rather than retrieving more data, we'll 
 learn the syntax used to add, update, and delete data.
 These data processing methods are used in all web 
 services such as online stores and social media.
 
+Adding Data
+INSERT:
+The INSERT statement is used when entering a new record into a table.
+
 
 
 
 #Progate @progateEN @dev_careers #dev_careers
+
+*/
