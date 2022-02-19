08e23f02064c13997e6fa2f5fa5625957f988ebe
arabambi <arababs2015@gmail.com>
2022-02-19 18:15:51 +0100

    SQL done

--- SQL/SQL5.sql
+++ SQL/SQL5.sql
@@ -72,4 +72,51 @@ SELECT * FROM students WHERE id=7;
 
 #Progate @progateEN @dev_careers #dev_careers
 
+
+The Four Statements
+Let's review the four statements we learned in this lesson. 
+When executing the UPDATE and DELETE statements, 
+you must always use WHERE to specify the record that 
+you'll be processing.
+
+
+
+GET DATA:
+          SELECT columnA, columnB
+          FROM table_name; 
+
+
+
+INSERT DATA:
+          INSERT  INTO table_name(columnA, columnB)
+          VALUES (value1, value2); 
+
+
+UPDATE DATA:
+          UPDATE table_name 
+          SET columnA = value1, columnB = value2
+          WHERE condition;    // alw specify
+
+
+DELETE DATA:
+          DELETE FROM table_name 
+          WHERE condition;    // alw specify
+
+
+-- Add a new record to the students table, where the name is Katy and the course is HTML
+INSERT INTO students(name, course) VALUES ("Katy", "HTML");
+
+-- Within the students table, update the name to Juliet and the course to Ruby, for the record that has the id 6
+UPDATE students SET name = "Juliet", course = "Ruby" WHERE id = 6;
+
+-- Within the students table, delete the record with the id 2
+DELETE FROM students WHERE id = 2;
+
+-- Do not delete the following query
+SELECT * FROM students;
+
+
+
+
+
 */
