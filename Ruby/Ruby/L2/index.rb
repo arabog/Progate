a8378ec42d31db5e4a46bc9b70785d79bc013a0d
arabambi <arababs2015@gmail.com>
2022-02-25 19:54:45 +0100

    L2 8 done

--- Ruby/Ruby/L2/index.rb
+++ Ruby/Ruby/L2/index.rb
@@ -178,17 +178,75 @@
 # symbols to get the value.
 
 # strings
-user = {
-          "name" => "john", 
+# user = {
+#           "name" => "john", 
 
-          "age" => 21
-}
-puts user["name"]
+#           "age" => 21
+# }
+# puts user["name"]
 
 # symbols
-user = {
-          :name => "john", 
+# user = {
+#           :name => "john", 
 
-          :age => 21
-}
-puts user[:name]
+#           :age => 21
+# }
+# puts user[:name]
+
+# The Shorthand Syntax for Symbol Keys
+# You can use the shorthand syntax of writing the hash 
+# key when using symbols. :key => can be abbreviated 
+# to key:
+
+# user = {
+#           :name => "john", 
+
+#           :age => 21
+# }
+
+
+# user = {
+#           name: "john", 
+
+#           age: 21
+# }
+
+# puts user
+
+# Even with the shorthand syntax, the keys are still symbols. 
+# Therefore, when you want to get the value, you must use 
+# a symbol as the key.
+
+# puts user[:name]
+
+# Hash Syntax
+# Let's review the ways to define hashes. As we've learned 
+# so far, there are three ways to define them. The most 
+# common way is to use the shorthand form, so make 
+# sure you remember its syntax!
+
+# strings as keys
+# user = {
+#           "name" => "john", 
+
+#           "age" => 21
+# }
+
+
+# symbols as keys
+# user = {
+#           :name => "john", 
+
+#           :age => 21
+# }
+
+# symbols as keys(shorthands)
+# user = {
+#           name: "john", 
+
+#           age: 21
+# }
+
+# exam = {subject: "Math", score: 80}
+
+# puts "#{exam[:subject]} score is #{exam[:score]}"
