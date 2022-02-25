07608cdf558e8aecabc4acf3df201754027e0add
arabambi <arababs2015@gmail.com>
2022-02-25 14:33:44 +0100

    L2 6 done

--- Ruby/Ruby/L2/index.rb
+++ Ruby/Ruby/L2/index.rb
@@ -66,14 +66,83 @@
 # On the other hand, variables defined outside of the each 
 # method can be used inside the each method. 
 
-message = "Hello"
+# message = "Hello"
 
-names = ['John', 'Bob', 'Kate']
+# names = ['John', 'Bob', 'Kate']
 
-names.each do |name|
-          puts name
-end
+# names.each do |name|
+#           puts name
+# end
 
 # puts name           #impossible
 
-puts message        #possible
+# puts message        #possible
+
+# Hashes  (ds is like obj)
+# Like arrays, hashes are used to manage multiple groups of data. 
+# The difference is that hashes use keys instead of index numbers. 
+# With hashes, a key is paired with a value to form one element.
+
+# Create a Hash
+# {key1 => value1, key2 => value2, ...} creates a hash. Hashes are 
+# enclosed in {}, while arrays are enclosed in []. Be sure to put => 
+# between the key-value pair, and a comma , between the elements.
+
+# {"name" => "john", "age": 21}
+
+# Assigning Hashes to Variables
+# Just like an array, a hash can be stored into a variable. puts 
+# user will simply print the defined hash
+
+# user = {
+#           "name" => "john", 
+
+#           "age" => 21
+# }
+
+# puts user
+
+# Getting the Value of an Element
+# You can get the value of an element using its key, by writing 
+# hash_name[key].
+
+# user = {
+#           "name" => "john", 
+
+#           "age" => 21
+# }
+
+# puts user["name"]
+
+# Updating an Element of a Hash
+# The value of an element in the hash can be updated using 
+# hash_name[key] = new_value.
+
+# user = {
+#           "name" => "john", 
+
+#           "age" => 21
+# }
+
+# user["age"] = 22
+
+# puts user
+
+
+# Adding an Element to a Hash
+# You can add an element to the hash using the same syntax 
+# as updating an element: hash_name["key"] = new_value. 
+# However, be aware that if you specify a key that already 
+# exists in the hash, you're updating the element, not adding it.
+
+user = {
+          "name" => "john", 
+
+          "age" => 21
+}
+
+puts user
+
+user["gender"] = "male"
+
+puts user
