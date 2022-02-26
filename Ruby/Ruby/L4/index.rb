0cdd52a68a26a8d3207379cce7896e1545e99fee
arabambi <arababs2015@gmail.com>
2022-02-26 03:30:56 +0100

    L4 1 done

--- Ruby/Ruby/L4/index.rb
+++ Ruby/Ruby/L4/index.rb
@@ -1,3 +1,10 @@
+# Ruby Classes
+# This is the second to last lesson of the Ruby course!
+# In this lesson, you'll be learning about classes,
+
+# Ruby Classes
+# This is the second to last lesson of the Ruby course!
+# In this lesson, you'll be learning about classes,
 
 
 
