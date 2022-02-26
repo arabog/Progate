b848ffc50c166394108c2721964d48465f7b4933
arabambi <arababs2015@gmail.com>
2022-02-26 02:31:10 +0100

    L3 3 done

--- Ruby/Ruby/L3/index.rb
+++ Ruby/Ruby/L3/index.rb
@@ -8,10 +8,10 @@
 # Let's take a look at how to write methods. By writing def method_name 
 # and end, and placing code in between them, 
 
-def introduce
-          puts "Hello"
-          puts "I am Ken the Ninja"
-end
+# def introduce
+#           puts "Hello"
+#           puts "I am Ken the Ninja"
+# end
 
 
 # Calling a Method
@@ -19,7 +19,7 @@ end
 # when you want to use them. You can call a method simply by writing 
 # the method name. 
 
-introduce
+# introduce
 
 # Defining a Method
 # Let's learn how to define a method from scratch. All you have to 
@@ -29,6 +29,39 @@ introduce
 # def method_name 
           # some code
 # end
+
+# Arguments
+# You can pass values to a method when you call it. These values 
+# are known as arguments, and can be used inside methods. For 
+# example, you can make the introduce method print different 
+# names based on the argument provided.
+
+# def method_name(parameter)
+          # some code
+# end
+
+# Calling a Method with a Parameter
+# When you want to call a method that has a parameter, you can pass an 
+# argument as follows: method_name(argument). The method will receive 
+# the argument, and pass it into the parameter. 
+
+# def method_name(parameter)
+          # some code
+# end
+
+# method_name(argument)
+
+# Using Parameters
+# Parameters can be used inside a method just like other variables. 
+
+def introduce(name)
+          puts "Hello"
+          puts "I am #{name}"
+end
+
+introduce("Ken the Ninja")
+introduce("Master Wooly")
+
 
 
 
