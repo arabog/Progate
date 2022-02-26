198ec7419e9ca92dfe78a00444a4df108e74629f
arabambi <arababs2015@gmail.com>
2022-02-26 02:21:02 +0100

    L3 1 done

--- Ruby/Ruby/L3/index.rb
+++ Ruby/Ruby/L3/index.rb
@@ -1,3 +1,25 @@
+# Ruby Methods
+# We'll be working with methods in this lesson.
+
+# Methods
+# A method lets you combine multiple lines of code into a single unit.
+
+# Defining a Method
+# Let's take a look at how to write methods. By writing def method_name 
+# and end, and placing code in between them, 
+
+def introduce
+          puts "Hello"
+          puts "I am Ken the Ninja"
+end
+
+
+# Calling a Method
+# Methods are not executed just by defining them. You need to call them 
+# when you want to use them. You can call a method simply by writing 
+# the method name. 
+
+introduce
 
 
 #Progate @progateEN @dev_careers #dev_careers
