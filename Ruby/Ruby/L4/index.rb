0e9044bed0f79ec0ed5b582a0dc457f0caefe946
arabambi <arababs2015@gmail.com>
2022-02-26 03:35:04 +0100

    L4 2 done

--- Ruby/Ruby/L4/index.rb
+++ Ruby/Ruby/L4/index.rb
@@ -6,6 +6,20 @@
 # This is the second to last lesson of the Ruby course!
 # In this lesson, you'll be learning about classes,
 
+# Creating an Object
+# To create an object, you first need to prepare its blueprint. 
+# In programming terms, this blueprint is commonly known 
+# as the class, and each object is known as an instance.
+
+# Defining a Class
+# Let's first prepare the blueprint by defining a class. Classes can 
+# be defined with the following syntax: class ClassName. Bear in 
+# mind that the class name has to start with a capital letter. Also, 
+# don't forget to close the class definition using end like in methods.
+
+# class Menu
+          # some code
+# end
 
 
 #Progate @progateEN @dev_careers #dev_careers
