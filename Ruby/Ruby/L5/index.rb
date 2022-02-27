208c1e19c28d34a813c046104cebcd674323a6cf
arabambi <arababs2015@gmail.com>
2022-02-27 06:54:34 +0100

    L5 1 done

--- Ruby/Ruby/L5/index.rb
+++ Ruby/Ruby/L5/index.rb
@@ -1,9 +1,16 @@
 # Class Inheritance
 # practical uses of classes 
 
+# Let's create the Food and Drink classes, and have them 
+# print different strings.
 
+# If the menu item is a food, we will display its calorie, 
+# but if it is a drink, we will display its volume!
 
-
+# Using an Existing Class
+# While we can create the classes from scratch, it's also 
+# possible to use the Menu class as the base. This way, 
+# we won't be repeating the same code in multiple places.
 
 
 
