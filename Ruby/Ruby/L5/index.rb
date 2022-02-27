5a08a5129eeb01430725d0c630ae4e191df6a4da
arabambi <arababs2015@gmail.com>
2022-02-27 07:05:08 +0100

    L5 3 done

--- Ruby/Ruby/L5/index.rb
+++ Ruby/Ruby/L5/index.rb
@@ -13,16 +13,48 @@
 # we won't be repeating the same code in multiple places.
 
 
+# Class Inheritance
+# Creating a class based on another class is known 
+# as inheritance. Child class is referring to the new class, 
+# while parent class is referring to the class used as a base. 
+# You can use inheritance as follows: class ChildClass < ParentClass.
+
+# require "./menu"
+
+# class Food < Menu 
+#           # sm code
+# end
 
 
+# How Inheritance Works
+# With class inheritance, the child class inherits all the instance 
+# variables and methods of the parent.
+
+# Instances of the Child Class
+# The child class has access to all the instance variables and 
+# methods of the parent class. In the example below, an 
+# instance of the Food class is calling the instance variables 
+# and methods of the Menu class.
 
 
+class Menu
+          attr_accessor :name
+
+          def info
+                    return "#{self.name} $#{self.price}"
+          end
+
+end
 
 
+class Food < Menu 
+          # sm code
+end
 
+food1 = Food.new(name: "Pizza", price: 8)
 
+food1.name
 
-
-
+food1.info
 
 #Progate @progateEN @dev_careers #dev_careers
