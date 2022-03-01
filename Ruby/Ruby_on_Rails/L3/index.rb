c8fc53007203483a3250b1fcca312e4a92da0d10
arabambi <arababs2015@gmail.com>
2022-03-01 11:43:24 +0100

    Rails L3 3 done

--- Ruby/Ruby_on_Rails/L3/index.rb
+++ Ruby/Ruby_on_Rails/L3/index.rb
@@ -15,11 +15,34 @@
 # date and time. The updated_at column will also be updated 
 # each time you make changes to the data.
 
+# Getting a Post with the find_by Method
+# ids are important since we will be using them to display 
+# a specific post in the Post details page.
 
 
+# The find_by Method
+# You can get a post with a specific id using the find_by method. 
+# The find_by method gets the data that meets certain conditions. 
+# You can get the data that matches the id value from the database 
+# using Model.find_by(column_name: value).
 
+Model.find_by(column_name: value)
 
+# Let's confirm that you can get a specific post from the 
+# database using the find_by method.
 
+rails console
+
+post = Post.find_by(id: 3)
+
+# After getting the data using the find_by method, let's get the 
+# content of the post and the time that it was posted.
+
+# Run post.content and get the post content.
+post.content 
+
+# Run post.created_at and get the post time.
+post.created_at
 
 
 
