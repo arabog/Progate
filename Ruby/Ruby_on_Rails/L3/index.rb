a28ac93cea2470e8f4919592fa7671421bbeb151
arabambi <arababs2015@gmail.com>
2022-03-01 11:30:20 +0100

    Rails L3 2 done

--- Ruby/Ruby_on_Rails/L3/index.rb
+++ Ruby/Ruby_on_Rails/L3/index.rb
@@ -1,5 +1,19 @@
+# Auto-Generated Columns
+# id, created_at, updated_at are automatically added when a 
+# table is created
 
 
+# The id Column
+# Let's look at the id column first. In the id column, a number 
+# is automatically assigned when the data is saved to the database. 
+# All ids are unique as they are incremented by 1 for each new 
+# record/row.
+
+# The created_at and updated_at Columns
+# When a new record gets saved, the created_at and the 
+# updated_at columns are automatically set to the current 
+# date and time. The updated_at column will also be updated 
+# each time you make changes to the data.
 
 
 
