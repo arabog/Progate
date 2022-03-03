de8c0abb808ffc67d9c8406f144bd4728130299f
arabambi <arababs2015@gmail.com>
2022-03-03 15:40:26 +0100

    Rails 4 L 2 done

---
+++ Ruby/Ruby_on_Rails/L4/index.rb
@@ -0,0 +1,24 @@
+# Editing and Deleting Data
+# In order to edit/delete a post, we need to work with the database.
+# Before we do so, let us first check the flow of editing and deleting 
+# using rails console.
+
+# Editing a Post
+# To edit a post, you need to 
+# 1. Get the post you want to edit; 
+# 2. Overwrite the content value of that post; 
+# 3. You can overwrite the value of the post's content 
+# by using post.content = "new value"
+
+rails console
+post = Post.find_by(id: 1)
+post.content = "Rails"
+post.save
+
+# updated_at
+# The created_at column and the updated_at column 
+# contain the date and time of when the post was created. 
+# If you edit the post data and save it to the database, the 
+# value of the updated_at column will change to the time 
+# when the data was most recently updated.
+
