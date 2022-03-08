a6df3093996d4572e860a94dd87afa61a34c739e
arabambi <arababs2015@gmail.com>
2022-03-08 18:19:36 +0100

    Rail L7 done2

---
+++ Ruby/Ruby_on_Rails/L8/index.rb
@@ -0,0 +1,15 @@
+# Creating the Login Feature
+# In this lesson, we will create the login/logout feature.
+# Let's master this essential mechanism to improve TweetApp!
+
+# What is Login?
+# Login is the process of letting the application know who 
+# is using the site. This will allow the application to display 
+# different content for each user.
+
+# Our Goal for This Lesson
+# 1. Login
+# 2. Logout
+# 3. Show name of d current user
+# 4. Limit access for users who r not logged In
+# 5. Prevent editing accounts of other users
