18246fcd498d169fc911dd58322e5c275bb09aac
arabambi <arababs2015@gmail.com>
2022-03-09 19:53:13 +0100

    Rails L8 4 done

--- Ruby/Ruby_on_Rails/L8/index.rb
+++ Ruby/Ruby_on_Rails/L8/index.rb
@@ -79,3 +79,40 @@ end
 user = User.find_by(id: 1)
 user.password = "Ibadan"
 user.save
+
+# Sending Form Values
+# we will send the content inputted into the form to the controller.
+# To accomplish this, we first need to add routes and actions.
+
+# Adding Routes and Actions
+# Let's add the appropriate routes and actions to receive values 
+# sent from the form. Note that a route should use the post method 
+# when sending values from a form.
+
+post "login" => "users#login"
+
+def login
+end
+
+# Setting Form Destination
+# Let's set the destination of the form in login_form.html.erb 
+# using the form_tag method. Let's also add the name 
+# attribute to the <input> tag so that values entered in the 
+# form are sent to the Rails app.
+
+<%= form_tag("/login") do %>
+          <p> Email </p>
+          <input name="email" >
+
+          <p> Password </p>
+          <input name="password" type="password" >
+# <% end %>
+
+# Routes for the login Action
+# It seems like the two routes with /login are the same, 
+# but get and post are treated as different routes. 
+# The link_to method looks for the get routing, while 
+# the form_tag method looks for the post routing.
+
+get "login" => "users#login_form"
+post "login" => "users#login"
