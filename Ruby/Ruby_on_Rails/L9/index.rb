a1cd1e430167b6444e5c4943ff1dd13da1e85994
arabambi <arababs2015@gmail.com>
2022-03-11 18:39:19 +0100

    L10 init done

---
+++ Ruby/Ruby_on_Rails/L9/index.rb
@@ -0,0 +1,6 @@
+# Associating Posts with a User
+# In this lesson, we will associate posts with a user. 
+
+# On the Posts page, we'll display the name of the 
+# ser who created each post. On the User details page, 
+# we'll display all the posts created by that user.
