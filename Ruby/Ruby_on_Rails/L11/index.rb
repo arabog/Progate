f3c69afc3479fe1636172d63698f1c92cb58af8a
arabambi <arababs2015@gmail.com>
2022-03-12 17:15:25 +0100

    Rails L11 2 done

--- Ruby/Ruby_on_Rails/L11/index.rb
+++ Ruby/Ruby_on_Rails/L11/index.rb
@@ -4,7 +4,37 @@
 
 #Progate @progateEN @dev_careers #dev_careers
 
-# :-
+# :- Editing the Gemfile
+# When encrypting passwords with Rails, something 
+# known as a gem is required.
+
+# gem
+# A gem is a third-party library you can use with Ruby. 
+# There are various gems such as "the search functionality 
+# gem", or "the encrypt passwords gem". This time, we'll 
+# use bcrypt, which is a gem to encrypt passwords.
+
+# Gemfile
+# In Rails, there's a file named Gemfile where you write 
+# the gems you want to install, like gem 'gem_name'. 
+# Some gems are already written when you run the rails 
+# new command. When you install gems, the Gemfile.lock 
+# file is automatically updated.
+
+# Gemfile
+gem 'rails', '5.0.3'
+gem 'puma', '3.6.2'
+gem 'sass-rails', '5.0.6'
+
+# Gem Versions
+# You can specify the version of the gem, like gem 'rails', '5.0.3'.
+# If you don't specify a version, the latest version of the gem will 
+# be installed.
+
+gem 'rails', '5.0.3' #specify d version
+
+gem 'puma' #install d latest version if noth is specified
+
 
 
 
