2b75b1c1e85b9a09c18b49971e252a18bc3aada5
arabambi <arababs2015@gmail.com>
2022-03-12 18:03:29 +0100

    Rails miscellaneous 1 done

---
+++ Ruby/Ruby_on_Rails/L12/miscellaneous.rb
@@ -0,0 +1,8 @@
+# -: Getting Started With Local Web Development
+# 1. Local Development Environment
+# First, let's break down the meaning of "local development environment":
+# Local environment → your computer
+# Development → to create something, such as a web application
+
+
+# -: Ruby on Rails Server Setup (macOS)
