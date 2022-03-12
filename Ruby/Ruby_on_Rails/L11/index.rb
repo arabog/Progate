8244ccae0e4ecf8879b8c362c015f220873d0c98
arabambi <arababs2015@gmail.com>
2022-03-12 17:20:51 +0100

    Rails L11 3 done

--- Ruby/Ruby_on_Rails/L11/index.rb
+++ Ruby/Ruby_on_Rails/L11/index.rb
@@ -35,14 +35,61 @@ gem 'rails', '5.0.3' #specify d version
 
 gem 'puma' #install d latest version if noth is specified
 
+# Installing bcrypt
+# This time, we'll add a gem known as bcrypt. You can do this 
+# by adding gem 'bcrypt' to the Gemfile, and run the command 
+# bundle install in the terminal.
+
+# Gemfile
+gem 'bcrypt'
+
+# terminal
+bundle install
+
+# :- has_secure_password
+has_secure_password
+# After installing bcrypt, you can use a method known as 
+# has_secure_password. By adding it to the User model, 
+# Rails will automatically encrypt the password when 
+# saving the user.
+
+# model/user.rb
+class User < ApplictaionRecord
+          has_secure_password
+end
 
 
+class User < ApplicationRecord
+          # Add the has_secure_password method
+          has_secure_password
+          
+          validates :name, {presence: true}
+          validates :email, {presence: true, uniqueness: true}
+          # Remove the following validation
+          validates :password, {presence: true}
+          
+          def posts
+                    return Post.where(user_id: self.id)
+          end
+          
+end
 
+# Next, let's delete the validation of the password column 
+# because the has_secure_password method automatically 
+# checks whether a password exists or not.
 
-
-
-# :-
-
+class User < ApplicationRecord
+          # Add the has_secure_password method
+          has_secure_password
+          
+          validates :name, {presence: true}
+          validates :email, {presence: true, uniqueness: true}
+          
+          def posts
+                    return Post.where(user_id: self.id)
+          end
+          
+end
 
 
 
