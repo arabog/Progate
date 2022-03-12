165aa1f5d707f16efcae0f08578f04f4eb70891d
arabambi <arababs2015@gmail.com>
2022-03-12 18:46:19 +0100

    Ruby on Rails done

--- Ruby/Ruby_on_Rails/L12/miscellaneous.rb
+++ Ruby/Ruby_on_Rails/L12/miscellaneous.rb
@@ -42,13 +42,100 @@ rails s
 Control + C
 
 
+# -: Tackling Local Web Application Development
+# You have finished your local development environment setup 
+# by following the environment setup article and are now ready 
+# to build your own web applications locally. Next let's think 
+# about the following:
+          1. What to create
+          2. The overall plan for your project
+
+# 1. Brainstorming What to Create
+# First, you will need to decide what you want to create. If you try 
+# to move forward without a clear vision, you will lose your sense 
+# of direction and become lost before reaching your goal.
+
+# However, it can be difficult to suddenly think of what to make. 
+# One thing you could do is develop your own version of an 
+# existing application. You may also be able to gain some 
+# inspiration by looking at your surroundings in your everyday 
+# life. Below are a few ideas you can start from:
+          # Recreate a website that you often use
+          # Alter a website that you are familiar with
+          # Think of a web application that may be useful in your 
+          # everyday life
+          # Recreate the project that you created throughout this path
+
+# 2. Outlining the Overall Plan
+# In this step, let's draw out your project plan. A rough 
+# sketch of the overall layout and screen transition diagram 
+# is enough to get started. It is a very rough diagram that 
+# depicts the application flow based on the user's actions.
+
+# By drawing out the overall project plan, you will be able 
+# to organize your project and also get a clearer picture of 
+# the necessary processes involved. When the project plan 
+# is detailed, it can also function as a blueprint, which 
+# allows for a smoother development process when you 
+# start to write out your code.
+
+# 3. Final Note
+# When developing applications in your own local 
+# environment, there aren't any instructions, hints, 
+# or answers like there are in the Progate lessons. 
+# You may encounter many errors and hurdles that 
+# prevent you from moving forward as planned. In 
+# times like these, please review the Progate lessons 
+# or look for information on the internet or in books 
+# and solve the problem through trial and error. It 
+# can be stressful and frustrating when things do not 
+# move according to plan. However, when you finally 
+# overcome these hurdles, you will be able to easily 
+# recognize and troubleshoot the next time you 
+# encounter similar problems.
 
 
-# -:
+# -: After the Web Development Course
+# This article will introduce some new courses and 
+# areas to be mindful of as you continue to grow and 
+# move forward in your programming journey.
+
+# Whether you wanted to land a job as a programmer, 
+# gain a new skill, or create something from scratch, 
+# you probably had certain goals or aspirations when 
+# you decided to begin your programming journey.
+
+# Your goal can be anything, such as:
+          # Creating a web application that everyone can use
+          # Developing a website for your friend's business
+          # Creating your own blog site from scratch
+
+# Please begin by setting a goal and continuing to push forward.
+
+# These are just a few possibilities:
+# Reviewing the Progate lessons
+# Creating an application using Ruby on Rails
+# Adding more features to your web application you 
+# created in your local environment
+
+# e.g) For the Tweet App
+# - Implementing an image submission function
+# - Implementing a user follow function
+
+# Rails Tutorial
+https://www.railstutorial.org/book
+
+# Reading Technical Books
+
+
+Programming and studying involves looking for answers 
+yourself. Make sure you always keep this in mind.
+
+Programming isn't something that you can just study once. It 
+is a journey where you must continue learning and growing.
 
 
 
-# -:
 
 
 
