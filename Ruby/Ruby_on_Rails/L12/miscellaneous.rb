d78ccd1d411a6325e25cdc50cd68b080639dd204
arabambi <arababs2015@gmail.com>
2022-03-12 18:14:25 +0100

    Rails miscellaneous 2 done

--- Ruby/Ruby_on_Rails/L12/miscellaneous.rb
+++ Ruby/Ruby_on_Rails/L12/miscellaneous.rb
@@ -6,3 +6,54 @@
 
 
 # -: Ruby on Rails Server Setup (macOS)
+# In this article, you will learn how to set up an environment 
+# for Ruby on Rails in your own computer.
+
+# In the Progate exercise screen, the Text Editor is the place 
+# where you enter code, and the Terminal is the place where 
+# you will see the result of the code execution.
+
+# 2. Ruby Versions
+# To check your version of Ruby, open the terminal and run the 
+# command below:
+rbenv versions
+
+# 3. Installing Ruby on Rails
+# Installing Rails is simple! Just run this command:
+gem install rails -v 5.2.6
+
+# Lastly, check your version of Rails. Run the command below:
+rails -v
+
+# 4. Creating a Rails App
+# Let's create a new Rails application. As you learned in the Rails 
+# lessons, use the command below to create a new application:
+rails new app_name
+
+# 5. Building a Local Rails Server
+# First, move to app_name directory by running the following command:
+cd app_name
+
+# Just as we introduced in the Rails lessons, the command below 
+# will build a Rails server:
+rails s
+
+# When you want to stop the Rails server, use the shortcut:
+Control + C
+
+
+
+
+# -:
+
+
+
+# -:
+
+
+
+# -:
+
+
+
+# -:
