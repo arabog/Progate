9df333a014ddf28b6c1ab777eeaa8632b3fa57a7
arabambi <arababs2015@gmail.com>
2022-03-16 11:52:19 +0100

    PHP L2 2 done

--- PHP/L2/index.php
+++ PHP/L2/index.php
@@ -21,6 +21,24 @@ Let's take a look at the food ordering site we're going to create.
 We'll take each menu as an object and create an ordering site 
 while assembling them.
 
+Classes
+To make the ordering site, we will now make objects for all of 
+the menus one by one. In object-oriented programming, we 
+create a blueprint of an object called "class" and create an 
+object (entity) based on the class (blueprint).
+
+How to Write Classes
+Now let's take a look at how to create classes (instance blueprint). 
+A class is defined as class ClassName and the contents of the 
+class are written between the { }. Note that the class name should 
+start with a capital letter.
+
+<?php
+          class Menu{
+                    // sm code
+          }
+?>
+
 
 
 
