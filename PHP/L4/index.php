34d079723b57eee5eaa9dfc8d28a617d44a74c34
arabambi <arababs2015@gmail.com>
2022-03-18 08:47:34 +0100

    PHP L4 3 done

--- PHP/L4/index.php
+++ PHP/L4/index.php
@@ -24,13 +24,51 @@ index.php
 </a>
 
 
+-: Giving Information to Link Destinations
+We created a link to go to the show.php page. Clicking any 
+menu will take you to the show.php page. However, the site 
+will not know which menu you clicked, making it impossible 
+to determine which menu details to display. We need to give 
+show.php information on which menu was clicked.
+
+Query Strings
+It's possible to put in simple information by typing 
+key=value after the ? at the end of the URL. This is 
+called a query string, and using this enables us to give 
+information to the link destination. By using query 
+strings, show.php will know which menu's details 
+page it should display.
+
+index.php
+<a href="show.php?name=<?php echo $curry -> getName() ?>" >
+          <?php echo $menu -> getName() ?>
+</a>
+
+$_GET
+In order to receive query strings, $_GET is used. The query 
+string is included inside $_GET as an associative array. 
+This means that a value can be taken out using $_GET['key'].
+
+index.php
+                                 key         value
+<a href="show.php?name=CURRY" >
+          CURRY
+</a>
+
+show.php
+echo $_GET['name']; //name specify d key to retrieve d value
+
+soln
+<?php
+          // Receive the query data using $_GET, and assign it to the $menuName variable
+          $menuName = $_GET['name'];
+?>
+
+<!-- Print the $menuName variable -->
+<h1><?php echo $menuName ?> details page</h1>
 
 
 
-
-
-
--:
 
 
 
