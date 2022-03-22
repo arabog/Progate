e50b14d9b39a756065c4526748969a5381000164
arabambi <arababs2015@gmail.com>
2022-03-22 20:29:36 +0100

    JS init

---
+++ JS/L1/index.js
@@ -0,0 +1,16 @@
+/*
+-: Calculation:
+Strings and Integers
+console.log(5 + 2); will print out the result of the addition, 
+which is 7. On the other hand, when you enclose 5 + 2 in 
+quotes, it'll be printed as a string. Remember that strings 
+and integers are different!
+
+
+Multiplication and Division:
+multiplication can be done using the asterisk * symbol and 
+division can be done using the slash / symbol.
+
+
+
+*/ 
