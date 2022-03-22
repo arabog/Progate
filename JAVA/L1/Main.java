4b61611ee89e46f2bf80473738a18ac1c6444531
arabambi <arababs2015@gmail.com>
2022-03-23 00:06:16 +0100

    Java L1 done

--- JAVA/L1/Main.java
+++ JAVA/L1/Main.java
@@ -235,6 +235,33 @@ int num2 = 3;
 
 System.out.println((double)num1 / num2);
 
+-: final project
+class Main {
+          public static void main(String[] args) {
+          // Assign a string to the name variable
+          String name = "Kate Jones";
+          
+          // Assign an integer to the age variable
+          int age = 27;
+          
+          // Assign a double to the height variable (i.e. 1.6)
+          double height = 1.6;
+          
+          // Assign a double to the weight variable (i.e. 50.0)
+          double weight = 50.0;
+          
+          // Calculate the BMI and assign it into the bmi variable
+          double bmi = weight / height / height;
+          
+          // Print name, age, height, weight, and BMI
+          System.out.println("My name is " + name);
+          System.out.println("I am " + age + " years old");
+          System.out.println("My height is " + height + " meters");
+          System.out.println("My weight is " + weight + " kilograms");
+          System.out.println("My BMI is " + bmi);
+
+}
+
 */ 
 
 // javac Main.java 
@@ -277,7 +304,7 @@ class Main {
                     int num1 = 13;
                     int num2 = 3;
 
-                    System.out.println((double)num1 / num2);
+                    System.out.println((double) num1 / num2);
           }
 }
 
