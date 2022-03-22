9632aa8ff988181ccafb9f4c3807ae6315142c38
arabambi <arababs2015@gmail.com>
2022-03-22 20:36:19 +0100

    JS L1 5 done

--- JS/L1/index.js
+++ JS/L1/index.js
@@ -15,6 +15,19 @@ Remainders
 Furthermore, the percentage % symbol can be used to find 
 the remainder when dividing. 
 
+-: Combining Strings
+The + symbol can also be used to combine, or concatenate strings, 
+as it's more generally known. "Ken" + " the Ninja" is printed as 
+Ken the Ninja.
+
+Differences between Strings and Integers
+Let's look at an example of the difference between strings and integers.
+"3" + "5" will become the string "35", but 3 + 5 will become 8.
+This is why it's important to remember that strings and integers are 
+completely different!
+
+
+
 
 
 */ 
