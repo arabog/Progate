4c27f31ab7cbab2eaeb97ba45105665a1242ef39
arabambi <arababs2015@gmail.com>
2022-03-27 07:23:57 +0100

    L4 1 done

--- Java/L4/Main.java
+++ Java/L4/Main.java
@@ -4,6 +4,16 @@ In this lesson we'll learn one of the most important programming paradigms,
 Object-Oriented Programming (OOP). Object-orientation is more than just 
 syntax — it's a way of thinking about how to create applications.
 
+Why Object-Oriented Programming?
+Object-oriented programming makes our lives much easier. The 
+larger a program becomes, the harder it gets to keep track of 
+everything. Object-oriented programming makes it easy to build 
+and modify even very large programs. So, it's essential for making 
+applications.
+
+
+
+
 
 */ 
 
