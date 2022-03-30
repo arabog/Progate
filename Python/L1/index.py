a81b296c4646a67058b0c23df6f2714aac03d246
arabambi <arababs2015@gmail.com>
2022-03-30 06:25:57 +0100

    py L3 done

--- Python/L1/index.py
+++ Python/L1/index.py
@@ -21,3 +21,19 @@ print("Hello Python");
 # use them to leave notes.
 
 
+# -: Integers
+# You can use numbers like integers in programming. Unlike strings, 
+# they don't need to be enclosed in quotes. You can add and subtract 
+# integers just like you do in math. 
+
+print(3 + 7);
+print(3 - 7);
+
+# Difference between Strings and Integers
+# Strings and integers are interpreted differently in programming. 
+# Like the images below, 3 + 5 will print 8, which is the result of 
+# the addition. However, if you enclose the addition in quotes and 
+# make it a string, the output will stay as 3 + 5.
+
+print(3 + 5);
+print("3 + 5");
