5acacb0249465c634ffb79d7b97f877eb263ec9b
arabambi <arababs2015@gmail.com>
2022-03-30 06:17:58 +0100

    py L2 done

--- Python/L1/index.py
+++ Python/L1/index.py
@@ -1,5 +1,23 @@
-About Python
-Python is a programming language that's simple and easy 
-to understand. It's used in web development, machine learning, 
-statistic processing, and more.
+# About Python
+# Python is a programming language that's simple and easy 
+# to understand. It's used in web development, machine learning, 
+# statistic processing, and more.
+
+# -: 
+# You can display characters by using print(). Characters inside 
+# the parentheses will be displayed in the console.
+
+print("Hello Python");
+
+# Strings
+# In Python, a sequence of characters, like "Hello Python", is 
+# known as a string. A string needs to be enclosed in single ' 
+# or double " quotes. The output will be the same either way. 
+# If you don't put the characters in quotes, you'll get an error.
+
+# Comments
+# Adding # at the beginning will make the entire line a comment.
+# Comments will not be executed when running code, so you can 
+# use them to leave notes.
+
 
