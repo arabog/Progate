f30b2c5195cc202d5034c0f009d002d2db839e20
arabambi <arababs2015@gmail.com>
2022-03-30 08:02:15 +0100

    py L17 done

--- Python/L1/index.py
+++ Python/L1/index.py
@@ -346,3 +346,42 @@ print('You will buy ' + input_count + 'apples')
 # a number. If you want to use it as an integer to do some calculation, 
 # you have to convert it to an integer,
 
+apple_price = 2
+
+# Receive the number of apples by using input(), and assign it to the input_count variable 
+input_count = input('How many apples do you want?: ')
+
+# Convert the input_count variable to an integer, and assign it to the count variable
+count = int(input_count)
+total_price = apple_price * count
+
+print('You will buy ' + str(count) + ' apples')
+print('The total price is ' + str(total_price) + ' dollars')
+
+# -: More Conditions!
+# Finally, let's use control flow to tell whether you have enough 
+# money to buy the specified number of apples.
+
+apple_price = 2
+# Assign 10 to the money variable
+money = 10
+
+input_count = input('How many apples do you want?: ')
+count = int(input_count)
+total_price = apple_price * count
+
+print('You will buy ' + str(count) + ' apples')
+print('The total price is ' + str(total_price) + ' dollars')
+
+# Add control flow based on the comparison of money and total_price
+money_left = str(money - total_price)
+
+if money > total_price:
+          print('You have bought ' + input_count + ' apples')
+          print('You have ' + money_left  + ' dollars left')
+elif money == total_price:
+          print('You have bought ' + input_count + ' apples')
+          print('Your wallet is now empty')
+else: 
+          print('You do not have enough money')
+          print('You cannot buy that many apples')
