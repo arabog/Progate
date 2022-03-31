a85b4ddcfcce08cd9eaa924b14ef80fd4af4ff09
arabambi <arababs2015@gmail.com>
2022-03-31 17:25:51 +0100

    L2 9 done

--- Python/L2/index.py
+++ Python/L2/index.py
@@ -180,3 +180,26 @@ while x <= 100:
 #           print(x)
 # x += 1
 
+
+# -: break
+# You can use the break statement to exit loops immediately. Break 
+# statements are usually combined with if statements. They can be 
+# used the same way in while loops.
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+for number in numbers:
+          print(number)
+
+          if number ==3:
+                    break     #d loop stops as soon as number bcoms 3
+
+numbers = [765, 921, 777, 256]
+
+for number in numbers:
+          print(number)
+
+          # When the number variable is 777, print '777 found, stopping loop' and exit the loop
+          if number == 777:
+                    print('777 found, stopping loop')
+                    break
