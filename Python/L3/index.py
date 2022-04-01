44ddb9473753d9f2501e1fa3141b0e3248937641
arabambi <arababs2015@gmail.com>
2022-04-01 03:26:42 +0100

    L3 2 done

--- Python/L3/index.py
+++ Python/L3/index.py
@@ -3,3 +3,31 @@
 # To keep things interesting, let's do this while creating a Rock 
 # Paper Scissors game.
 
+
+# -: Functions
+# First of all, we'll take a look how functions work in Python. A 
+# function is a piece of code that executes a specific task. print 
+# is one example of a function. It allows you to print text easily 
+# without needing to write a lot of code.
+
+# Defining a Function
+# Let's learn how to define a basic function. The syntax for defining 
+# a function is as follows: def function_name():. Keep in mind that 
+# you need to indent the code inside the function.
+
+# def function_name:
+#           sm code 
+
+# def greet():
+#           print('Hello')
+
+# Calling Functions
+# Since functions aren't executed just by defining them, you need 
+# to call them to execute the code in the function. To call a function, 
+# simply write function_name(). Keep in mind that a function can 
+# only be called after it's been defined.
+
+def greet():
+          print('Hello')
+
+greet()
