7979582a3d9bc0d2de2ef78394411c8aa8c840e4
arabambi <arababs2015@gmail.com>
2022-04-02 05:39:57 +0100

    L4 8 done

--- Python/L4/index.py
+++ Python/L4/index.py
@@ -139,3 +139,26 @@ menu_item1.info()
 age = 21
 print('Age: ' + str(age))
 
+
+# -: Using Instance Methods
+class MenuItem:
+          def info(self):
+                    # Return the contents of print() as a return value
+                    return self.name + ': $' + str(self.price)
+
+
+menu_item1 = MenuItem()
+menu_item1.name = 'Sandwich'
+menu_item1.price = 5
+
+# Output the value of menu_item1.info()
+print(menu_item1.info())
+
+menu_item2 = MenuItem()
+menu_item2.name = 'Chocolate Cake'
+menu_item2.price = 4
+
+# Output the value of menu_item2.info()
+print(menu_item2.info())
+
+
