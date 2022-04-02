722a198a9f463bb66619a57d636ba6f44b71d20e
arabambi <arababs2015@gmail.com>
2022-04-02 05:06:41 +0100

    L4 2 done

--- Python/L4/index.py
+++ Python/L4/index.py
@@ -1,1 +1,28 @@
 #Classes
+# -: Creating Objects
+# To make objects for the menu items, we first need to prepare a 
+# blueprint. Blueprints are known as classes, and their objects are 
+# known as instances.
+
+# Steps to Creating Instances
+# The process to creating an instance can be seen below. We'll 
+# start from 
+# Step 1: Prepare a Class 
+# Step 2: Create an Instance of the Class
+# Step 3: Add Data to the Instance
+
+# Defining a Class
+# Let's first prepare the class (blueprint). A class can be defined 
+# using class ClassName:. Class names, by convention, always begin 
+# with a capital letter 
+
+# class MenuItem:
+
+# Class Contents
+# The contents of the class (or blueprint instructions) are written 
+# starting the next line under class MenuItem: and must be indented. 
+# Since we still don't need to add anything yet, we'll write pass, 
+# which we use when we have no code to execute 
+
+class MenuItem:
+          pass
