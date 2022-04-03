693582385b8841ac0157d7207bcc5c5eafba9549
arabambi <arababs2015@gmail.com>
2022-04-03 08:28:23 +0100

    J6 L1 done

---
+++ JS/L6/index.js
@@ -0,0 +1,21 @@
+/*
+Array Methods
+Let's learn about methods for working with arrays! With these methods, 
+you'll be able to easily handle data with JavaScript.
+The methods we'll cover in this lesson are very useful and essential knowledge 
+for practical JavaScript development.
+
+push
+The push method is used to add a new value to an array.
+Put the new value you want to add to an array in () right after the word 
+push. The example below adds the value 4 to the end of the numbers array 
+by passing it as an argument to the push method.
+
+const num = [1, 2, 3];
+num.push(4);
+
+
+
+
+
+*/ 
