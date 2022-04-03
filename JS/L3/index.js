92b9f57ce96ba5ccb44d3d57614fc43081c16aea
arabambi <arababs2015@gmail.com>
2022-04-03 08:12:50 +0100

    J3 L8 done

--- JS/L3/index.js
+++ JS/L3/index.js
@@ -139,6 +139,34 @@ if ( check(123) ) {
 }
 
 
+-: Scope
+Constants inside Functions
+Parameters of a function and constants defined within a function can 
+only be used within that function. In the example below, the name 
+constant can only be used within the introduce function because it's 
+defined inside the function. If you try to use the name constant outside 
+of the function, it'll cause an error.
+
+const introduce = () => {
+          const name = 'Ken the Ninja';
+}
+
+Constants Outside Functions
+If a constant is defined outside of a function, it can be used both inside 
+and outside of it. In the example below, the name constant is defined 
+outside of the introduce function, so it can be used inside and outside 
+of the function.
+
+const name = 'Ken the Ninja';
+const introduce = () => {}
+
+Scope of Constants
+Each constant has a part of a program where it can be accessed, and 
+we call this the scope of a constant.
+
+
+
+
 
 
 */
