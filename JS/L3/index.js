a815fe0a50b89e2205b1e5b58ff64c313a157891
arabambi <arababs2015@gmail.com>
2022-04-03 08:07:30 +0100

    J3 L7 done

--- JS/L3/index.js
+++ JS/L3/index.js
@@ -103,6 +103,43 @@ const sum = add(1, 3);
 console.log(sum);
 
 
+-: Return Value Types
+Just like arguments, you can use different value types for return values.
+For example, a boolean (true or false) value will be returned if you use 
+a conditional statement like the conditions used in if statements.
+
+const check = (number) => {
+          return number % 2 === 0;
+}
+
+console.log(check(6));
+console.log(check(7));
+
+Ending a Function with Return
+Let's learn about return statements in more detail. You can also use 
+return to exit a function. However, it's important to note that the code 
+after return won't run.
+
+const add = (a, b) => {
+          return a + b;
+
+          console.log('Calculated');              //doesn't run
+}
+
+const check = (number) => {
+          return number % 3 === 0;
+};
+
+
+// Call the check function in the condition of the if statement
+if ( check(123) ) {
+          console.log("Multiple of 3");
+} else {
+          console.log("Not a multiple of 3");
+}
+
+
+
 
 */
 
