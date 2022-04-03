eff922cfb636521202a8e5284c53a1e7036bc452
arabambi <arababs2015@gmail.com>
2022-04-03 13:17:53 +0100

    L7 2 done

--- JS/L7/index.js
+++ JS/L7/index.js
@@ -21,9 +21,63 @@ call(printKen);
 call(printMaster);
 
 
+-: What Is a Callback Function?
+Functions that are passed to other functions as arguments are 
+called callback functions.
 
+Argument Types
+In JavaScript, functions can be passed as arguments.
+Functions that are passed as arguments are called callback functions.
+Just like strings, integers and booleans can be passed as argument
+of a function.
 
+function(argument);
 
+Passing Callback Functions
+As you can see in the figure below, the function printKen is passed 
+as an argument when the function call is called.
+
+const call = (parameterName) => {}
+
+call(functionName);
+
+const printKen = () => {}
+
+const call = (callback) => {}
+call(printKen);
+
+Calling and Passing Functions
+Functions are called by putting () after the function. Without (), 
+the function simply points to its definition. Therefore, calling 
+and passing functions are written differently.
+
+const printKen = () => {
+          console.log('Ken the Ninja')
+}
+
+const call = (callback) => {
+          console.log('Calling the callback function');
+
+          callback();         //calling d callback
+}
+
+call(printKen);     //don't add () wn passing d callback fxn
+
+Flow of Callback Functions
+Let us take a look at the process of calling the function printKen, 
+which is passed as an argument to call.
+
+const printKen = () => {
+          console.log('Ken the Ninja')
+}
+
+const call = (callback) => {                      (2)
+          console.log('Calling the callback function');
+
+          callback();                   (3)
+}
+
+call(printKen);     (1)
 
 
 
