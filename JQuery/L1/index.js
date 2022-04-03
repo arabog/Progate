4ee1c52f4e755a954123631c5409e804d3707b4f
arabambi <arababs2015@gmail.com>
2022-04-03 13:43:00 +0100

    JQ L1 1 done

---
+++ JQuery/L1/index.js
@@ -0,0 +1,15 @@
+/*
+Getting Started with jQuery!
+Welcome to the world of jQuery!
+The purpose of jQuery is to make it much easier to use JavaScript 
+for building websites. You can create animations and design interactive 
+web pages using jQuery.
+
+jQuery, the JS Library
+jQuery is a JavaScript library. It allows you to create various 
+animation effects that HTML and CSS cannot accomplish alone. 
+For example, jQuery can respond to events like clicks by hiding 
+and showing HTML elements.
+
+
+*/ 
