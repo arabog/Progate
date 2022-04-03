2cbcfb9f305decc610f2b892d669487d7206d0ff
arabambi <arababs2015@gmail.com>
2022-04-03 07:40:26 +0100

    J3 L1 done

---
+++ JS/L3/index.js
@@ -0,0 +1,25 @@
+/* What’s a Function?
+A function is a set of instructions.
+
+Defining a Function
+To make a function, you need to define it. To define a function, 
+writefunction() and then write what you want the function to do 
+inside the braces {}
+
+const functionName = function() {}
+
+Calling Functions
+Function definitions are necessary, but you have to call a function 
+to actually use it. To call the function in the example below, just 
+write introduce();.
+
+
+
+*/
+
+const introduce = function() {
+          console.log('Hello');
+          console.log('I am Ken the Ninja');
+}
+
+introduce();
