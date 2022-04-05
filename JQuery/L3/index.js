7f8271d638d7bfafc93c4bd90aa4353ed968ce33
arabambi <arababs2015@gmail.com>
2022-04-05 18:08:53 +0100

    jQ L3 5 done

--- JQuery/L3/index.js
+++ JQuery/L3/index.js
@@ -177,6 +177,77 @@ $('#center').next().css('color', 'blue');
 });
 
 
+-: Hiding Buttons
+You may want to hide the "Previous" button when the first slide is 
+displayed and the "Next" button when the last slide is displayed.
+
+Conditional Branching
+In order to decide whether each button is displayed, let's make c
+onditional branching as shown below. We will implement slide 
+index numbers and combinations of if... else if
+
+if( d 1st slide is displayed) {
+          // hide d 'prev' btn
+} else if (d last slide is displayed) {
+          // hide d 'next' btn
+}
+
+$(function() {
+          $('.index-btn').click(function() {
+                    $('.active').removeClass('active');
+                    var clickedIndex = $('.index-btn').index(this);
+                    $('.slide').eq(clickedIndex).addClass('active');
+
+                    // 1. Store the index number of the .active element in the slideIndex variable
+                    var slideIndex = $('.slide').index($('.active'));
+
+                    // 3. Display the change-btn element
+                    $('.change-btn').show();
+
+                    // 2. Use an if-else statement to hide/show the .change-btn element
+                    if(slideIndex == 0) {
+                              $('.prev-btn').hide();
+                              $('.next-btn').show();
+                    } else if (slideIndex == 3) {
+                              $('.prev-btn').show();
+                              $('.next-btn').hide();
+                    }
+          });
+
+          $('.change-btn').click(function() {
+                    var $displaySlide = $('.active');
+                    $displaySlide.removeClass('active');
+
+                    if ($(this).hasClass('next-btn')) {
+                              $displaySlide.next().addClass('active');
+                    } else {
+                              $displaySlide.prev().addClass('active');
+                    }
+
+                    // Write the same process as the click event for index-btn
+                    var slideIndex = $('.slide').index($('.active'));
+
+                    // 3. Display the change-btn element
+                    $('.change-btn').show();
+
+                    // 2. Use an if-else statement to hide/show the .change-btn element
+                    if(slideIndex == 0) {
+                              $('.prev-btn').hide();
+                              $('.next-btn').show();
+                    } else if (slideIndex == 3) {
+                              $('.prev-btn').show();
+                              $('.next-btn').hide();
+                    }
+          });
+});
+
+
+
+
+
+
+
+
 */ 
 
 // Making Navigation Bar
