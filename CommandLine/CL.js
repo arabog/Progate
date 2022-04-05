6fa2d154bec939c6cee09c6010ab1920e73c9ac5
arabambi <arababs2015@gmail.com>
2022-04-05 22:32:39 +0100

    CL L1 done

---
+++ CommandLine/CL.js
@@ -0,0 +1,24 @@
+/*
+Command Line
+Let's learn the fundamental UNIX commands necessary for 
+development in this course!
+
+Command Line
+The command line is a tool for interacting with a computer using 
+only text (also known as a text interface) rather than other methods 
+like clicking and scrolling.UNIX command is a type of command 
+that’s used in Linux and macOS.
+
+Creating a File
+First, let's look at the command for creating new files, the touch 
+command. You can create an empty file by typing touch file_name 
+and executing it.
+
+touch beginner.js
+
+
+
+
+
+
+*/ 
