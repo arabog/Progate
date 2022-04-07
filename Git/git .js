a2d595fa497239beaa9d0f53aad2e365bcd55e4f
arabambi <arababs2015@gmail.com>
2022-04-07 20:28:41 +0100

    git L1 done

---
+++ Git/git .js
@@ -0,0 +1,16 @@
+/*
+Let's Learn Git
+When making a website, you may have to collaborate with others.
+Git is a tool that'll help you to do team development smoothly.
+
+Git Setup
+Git is operated from the terminal.
+First, to get started, you need to run the command git init.
+
+git init
+init is short for initialize
+
+
+
+
+*/ 
