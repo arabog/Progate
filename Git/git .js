c82305809694d9469aec44961dde76595cabf632
arabambi <arababs2015@gmail.com>
2022-04-07 20:52:43 +0100

    git L2 done

--- Git/git .js
+++ Git/git .js
@@ -11,6 +11,35 @@ git init
 init is short for initialize
 
 
+-: The Role of Git
+These are the 3 steps to team development.
+1. Make changes to a file
+2. Prepare to share d changes
+3. Share d change
+
+Selecting Files to Share
+To select a file, use the command git add file_name.
+
+git add index.html
+
+
+-: Recording Selected Files
+We will save the selected files with a message. Then we are 
+ready to share them.
+
+git commit
+
+To record a selected file, run the command git commit -m "message"
+We call this kind of saving a commit. Well, it should describe the 
+content of the commit. By the way, this message is called a 
+commit message.
+
+git commit -m 'create index.html'
+
+With git add and git commit, the 2 commands we've 
+already learned, you are ready to share files.
+
+
 
 
 */ 
